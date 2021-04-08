86160779f2bc4e16081b79302879cc115998e80b
daricocity <daricocity@gmail.com>
2021-04-08 18:19:13 +0100

    Update urls.py

--- chats/urls.py
+++ chats/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import GenericFileUploadView, MessageView
+from .views import GenericFileUploadView, MessageView, ReadMultipleMessages
 
 router = DefaultRouter(trailing_slash = False)
 router.register('message', MessageView)
@@ -8,4 +8,5 @@ router.register("file-upload", GenericFileUploadView)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("read-messages", ReadMultipleMessages.as_view()),
 ]
